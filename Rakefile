b86828e9cd87d1655acc939ed79bf30c51a27235
Jacek Becela <jacek.becela@gmail.com>
2010-04-22 11:17:19 +0200

    Initial commit

---
+++ Rakefile
@@ -0,0 +1,16 @@
+begin
+  require 'jeweler'
+  Jeweler::Tasks.new do |gem|
+    gem.name = "rack-cat"
+    gem.summary = "Rack middleware to concatenate yor assets (static, dynamic and remote)"
+    gem.description = "Rack middleware to concatenate yor assets (static, dynamic and remote). Use it to serve your javascripts and stylesheets faster!"
+    gem.email = "jacek.becela@gmail.com"
+    gem.homepage = "http://github.com/ncr/rack-cat"
+    gem.authors = ["Jacek Becela"]
+    gem.add_dependency "rack"
+    gem.add_development_dependency "rack-test"
+  end
+  Jeweler::GemcutterTasks.new
+rescue LoadError
+  puts "Jeweler not available. Install it with: gem install jeweler"
+end
---
+++ lib/rack/cat.rb
@@ -0,0 +1,53 @@
+require "rack/mock"
+require "fileutils"
+
+module Rack
+  class Cat
+    def initialize(app, options = {})
+      @app         = app
+      @bundles     = options[:bundles]     # { "/destination/path" => [ "/array/of", "/source/paths"] }
+      @destination = options[:destination] # a directory to write bundles into
+      @sources     = options[:sources]     # array with source dirs
+      @debug       = options[:debug]       # regenerate bundles on each request
+      
+      create_bundles
+    end
+    
+    def call(env)
+      create_bundles if @debug
+      @app.call(env)
+    end
+    
+    private
+    
+    def create_bundles
+      @bundles.each do |bundle, paths|
+        concatenation = paths.map do |path|
+          read_from_disk(path) || read_from_app(path)
+        end.join("\n")
+        
+        write_to_disk(bundle, concatenation)
+      end
+    end
+    
+    def write_to_disk(path, content)
+      full_path = File.join(@destination, path)
+      FileUtils.mkdir_p(File.dirname(full_path))
+
+      File.open(full_path, "w") do |f|
+        f.write(content)
+      end
+    end
+    
+    def read_from_disk(path)
+      @sources.each do |source|
+        full_path = File.join(source, Rack::Utils.unescape(path))
+        return File.read(full_path) if File.file?(full_path) && File.readable?(full_path)
+      end
+    end
+
+    def read_from_app(path)
+      Rack::MockRequest.new(@app).get(path).body
+    end
+  end
+end
